d13eecc3d60beab62035ab7f24755237c8711c63
marmalou <marmalou2@gmail.com>
2019-07-11 14:21:18 +0100

    created classes

--- start_code/models/movie.rb
+++ start_code/models/movie.rb
@@ -1,3 +1,14 @@
+require_relative('../db/sql_runner')
+
 class Movie
 
+attr_reader :id
+atrr_accessor :title, :genre
+
+def initialize(options)
+@id = options ["id"].to_id if options ['id']
+@title = options ["title"]
+@genre = options ["genre"]
 end
+
+end 
