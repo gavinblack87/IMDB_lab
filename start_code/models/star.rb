e9535b0fe70599e07898c2f385af697d92068b8f
marmalou <marmalou2@gmail.com>
2019-07-11 15:23:53 +0100

    crud updates

--- start_code/models/star.rb
+++ start_code/models/star.rb
@@ -12,6 +12,46 @@ def initialize(options)
  @last_name = options ["last_name"]
 end
 
+def save()
+  sql = "INSERT INTO stars
+  (first_name,
+  second_name
+    ) VALUES ( $1, $2 ) RETURNING id "
+values = [@first_name, @second_name]
+star = SqlRunner.run (sql, values).first
+@id = user ['id'].to_i
+end
+
+def update()
+  sql = 'UPDATE stars SET (
+  first_name,
+  last_name)=
+  (
+    $1, $2
+  )
+  WHERE id = $3'
+  values = [@first_name, @last_name, @id]
+  SqlRunner.run(sql, values)
+end
+
+def self.all()
+  sql = 'SELECT * FROM stars'
+  star_data = SqlRunner.run(sql)
+  result = star_data.map{ |star| Star.new(star)}
+  return result
+end
+
+def self.delete_all()
+  sql = 'DELETE FROM stars'
+  SqlRunner.run(sql)
+end
+
+
+
+
+
+
+
 
 
 end
